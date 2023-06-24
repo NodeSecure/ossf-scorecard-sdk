9b240732113caba0eecff61790bf2a26c1428eab
PierreDemailly <pierredemailly.pro@gmail.com>
2023-06-24 14:06:15 +0200

    refactor: improve code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 // Import Third-party Dependencies
 import { fetch } from "undici";
 import { packument } from "@nodesecure/npm-registry-sdk";
-import { Octokit } from "octokit";
-import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 
 // Import Internal Dependencies
 import { repositoryFromUrl } from "./utils/repositoryFromUrl.js";
 
 // CONSTANTS
 const kDefaultPlatform = "github.com";
+const kGitHubApiUrl = "https://api.github.com";
 export const API_URL = "https://api.securityscorecards.dev";
 
 export type ScorecardCheck = {
@@ -45,6 +44,20 @@ export interface IResultOptions {
   platform?: string;
 }
 
+async function getNpmRepository(repository: string): Promise<string> {
+  const data = await packument(repository);
+  const latestVersion = data["dist-tags"].latest;
+
+  if (latestVersion) {
+    const repository = data.versions[latestVersion].repository;
+    const url = typeof repository === "string" ? repository : repository?.url;
+
+    return repositoryFromUrl(url ?? "");
+  }
+
+  throw new Error("Cannot find the latest version of the given repository");
+}
+
 /**
  * @description Get a repository's ScorecardResult
  * @see https://api.securityscorecards.dev/#/results/getResult
@@ -56,29 +69,25 @@ export async function result(
   let formattedRepository = repository;
   const { platform = kDefaultPlatform } = options;
   const [owner, repo] = repository.replace("@", "").split("/");
-  const octokit = new (Octokit.plugin(restEndpointMethods))({ auth: process.env.GITHUB_TOKEN });
 
   try {
     // We try to retrieve the repository with the GitHub API because
     // Scorecard API is case sensitive, i.e if we pass "nodesecure/cli",
     // we must reformat it to "NodeSecure/cli"
-    const { data } = await octokit.rest.repos.get({
-      owner,
-      repo
-    });
+    const response = await fetch(
+      new URL(`/repos/${owner}/${repo}`, kGitHubApiUrl)
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json() as any;
     formattedRepository = data.full_name;
   }
   catch {
     // If the repository is not found, we try to retrieve it from the NPM registry
     // i.e: if given repository is "@nodesecure/cli"
     try {
-      const data = await packument(repository);
-      const latestVersion = data["dist-tags"].latest;
-      if (latestVersion) {
-        const repository = data.versions[latestVersion].repository;
-        const url = typeof repository === "string" ? repository : repository?.url;
-        formattedRepository = repositoryFromUrl(url ?? "");
-      }
+      formattedRepository = await getNpmRepository(repository);
     }
     catch {
       throw new Error("Invalid repository, cannot find it on GitHub or NPM registry");
--- src/utils/repositoryFromUrl.ts
+++ src/utils/repositoryFromUrl.ts
@@ -1,23 +1,22 @@
 // https://docs.npmjs.com/cli/v9/configuring-npm/package-json#repository
 export function repositoryFromUrl(url: string): string {
-  if (url.startsWith("gist:") || url.includes("gist.github.com")) {
-    throw new Error("Cannot get repository from a gist url");
+  try {
+    const { protocol, host, href, pathname } = new URL(url);
+    if (protocol === "gist:" || href.startsWith("gist") || host.startsWith("gist")) {
+      throw new Error("Cannot get repository from a gist url");
+    }
+
+    if (pathname.startsWith("/")) {
+      return pathname.slice(1).replace(".git", "");
+    }
+
+    return pathname.replace(".git", "");
   }
+  catch (error) {
+    if (error.code === "ERR_INVALID_URL") {
+      return url;
+    }
 
-  if (url.startsWith("github:") || url.startsWith("bitbucket:") || url.startsWith("gitlab:")) {
-    return url.split(":")[1];
+    throw error;
   }
-
-  if (!url.includes("http") && !url.includes(".git")) {
-    return url;
-  }
-
-  const formattedUrl = url.replace("http:", "https:");
-  const { pathname } = new URL(formattedUrl);
-
-  if (pathname.endsWith(".git")) {
-    return pathname.slice(1).replace(".git", "");
-  }
-
-  return pathname;
 }
